76d1fea7c3b682b177c315a1940f454b342822d9
Taylor Sargent <sarg0049@algonquinlive.com>
2016-03-11 10:40:11 -0500

    added js items to li tags

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var $ul = $('ul');
 
-grains.forEach(function (grain){
+grains.forEach(function (grain) {
   var $name = $('<h2>');
   var $img = $('<img>');
   var $desc = $('<p>');
-
+  var $li = $('<li>');
 
   $img.attr('src', 'images/' + grain.img);
   $name.html(grain.name);
   $desc.html(grain.desc);
 
-
-  $('body').append($img);
-  $('body').append($name);
-  $('body').append($desc);
+  $li.append($img);
+  $li.append($name);
+  $li.append($desc);
+  $ul.append($li);
 });
